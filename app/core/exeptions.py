dc154f5aa44c6d8ede33652519ccc68ab6e73672
KikoTheFinker <kristijanstojanovskii24@gmail.com>
2024-11-16 16:57:42 +0100

    Add User not found and Incorrect credentials exeptions

---
+++ app/core/exeptions.py
@@ -0,0 +1,15 @@
+from fastapi import HTTPException, status
+
+
+def raise_user_not_found():
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="User not found"
+    )
+
+
+def raise_incorrect_credentials():
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Incorrect credentials"
+    )
