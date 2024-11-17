f7e19b3720444495192e49fb20bc6f7328b4ce9b
KikoTheFinker <kristijanstojanovskii24@gmail.com>
2024-11-17 17:46:19 +0100

    Add item create and item update class

--- app/schemas/item.py
+++ app/schemas/item.py
@@ -2,21 +2,41 @@ from pydantic import BaseModel
 from typing import List
 from .category import CategoryResponse
 from .wardrobe import WardrobeResponse
-from .photo import PhotoResponse
+from .photo import PhotoCreate, PhotoResponse
 
 
-class Item(BaseModel):
-    item_id: int
+class ItemBase(BaseModel):
     name: str
     price: int
     is_price_fixed: bool
     is_for_rent: bool
 
 
-class ItemResponse(Item):
+class ItemResponse(ItemBase):
+    item_id: int
     category: CategoryResponse
     wardrobe: WardrobeResponse
     photos: List[PhotoResponse] = []
 
     class Config:
         orm_mode = True
+
+
+class ItemCreate(BaseModel):
+    name: str
+    price: int
+    is_price_fixed: bool
+    is_for_rent: bool
+    category: int
+    wardrobe: int
+    photos: List[PhotoCreate]
+
+
+class ItemUpdate(BaseModel):
+    name: str | None = None
+    price: int | None = None
+    is_price_fixed: bool | None = None
+    is_for_rent: bool | None = None
+    category_id: int | None = None
+    wardrobe_id: int | None = None
+    photo_ids: List[int] | None = None
