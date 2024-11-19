3596f3fa0bf212eee84cc96a72267fa1ce50d479
KikoTheFinker <kristijanstojanovskii24@gmail.com>
2024-11-19 22:51:06 +0100

    Add get_db function

--- app/db/database.py
+++ app/db/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Session
 
 DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/mosh"
 
@@ -10,3 +10,11 @@ engine = create_engine(
 SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
 
 Base = declarative_base()
+
+
+def get_db():
+    db: Session = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
