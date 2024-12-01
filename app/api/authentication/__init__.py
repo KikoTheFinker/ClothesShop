3d6d37c42e50635378b9f412f733864869ca9f24
KikoTheFinker <kristijanstojanovskii24@gmail.com>
2024-12-01 14:41:58 +0100

    Add all authentication endpoints

--- app/api/authentication/__init__.py
+++ app/api/authentication/__init__.py
@@ -1,9 +1,11 @@
 from app.api.authentication.register import router as register_router
 from app.api.authentication.login import router as login_router
 from app.api.authentication.logout import router as logout_router
+from app.api.authentication.status import router as status_router
 
 routers = [
     (register_router, "/api/authentication", ["authentication"]),
     (login_router, "/api/authentication", ["authentication"]),
     (logout_router, "/api/authentication", ["authentication"]),
+    (status_router, "/api/authentication", ["authentication"]),
 ]
