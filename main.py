292b238384dd5ee8803a04a3563118066cf9be58
KikoTheFinker <kristijanstojanovskii24@gmail.com>
2024-12-09 10:18:05 +0100

    Include item routers

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.authentication import routers as auth_routers
 from app.api.wardrobe import routers as wardrobe_routers
+from app.api.item import routers as item_routers
 
 app = FastAPI()
 
@@ -20,4 +21,7 @@ for router, prefix, tags in auth_routers:
     app.include_router(router, prefix=prefix, tags=tags)
 
 for router, prefix, tags in wardrobe_routers:
+    app.include_router(router, prefix=prefix, tags=tags)
+
+for router, prefix, tags in item_routers:
     app.include_router(router, prefix=prefix, tags=tags)
